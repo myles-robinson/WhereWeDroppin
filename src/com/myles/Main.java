49d0be4e97245b3f6195c87b579939d589dbf906
Myles Robinson <myles.robinson5@gmail.com>
2018-10-05 14:03:32 -0400

    Idk what happened

--- src/com/myles/Main.java
+++ src/com/myles/Main.java
@@ -1,26 +1,58 @@
 package com.myles;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Location> places = new ArrayList<>();
+        List<Location> locationsFromMap = Arrays.asList(new Location("Castle (POI)", 4),
+            new Location("Durrr Burger", 3),
+            new Location("Dusty Divot", 3),
+            new Location("Fatal Fields",2),
+            new Location("Floating Island", 5),
+            new Location("Flush Factory", 6),
+            new Location("Greasy Grove",1),
+            new Location("Haunted Hills",2),
+            new Location("Junk Junction",2),
+            new Location("Lazy Links",3),
+            new Location("Lonely Lodge",4),
+            new Location("Loot Lake",3),
+            new Location("Lucky Landing",2),
+            new Location("Paradise Palms",3),
+            new Location("Pleasant Park",4),
+            new Location("Retail Row",5),
+            new Location("Risky Reels",3),
+            new Location("Salty Springs",3),
+            new Location("Shifty Shafts",2),
+            new Location("Snobby Shores",4),
+            new Location("Tilted Towers",3),
+            new Location("Tomato Temple",3),
+            new Location("Wailing Woods",6)
+        );
 
-        Location tiltedTowers = new Location("Tilted Towers", 5);
-        Location moistyMire = new Location("Moisty Mire", 3);
-        Location snobbyShores = new Location("Snobby Shores", 1);
-        Location dustyDepot = new Location("Dusty Depot", 5);
+        Random rand = new Random();
 
-        places.add(tiltedTowers);
-        places.add(moistyMire);
-        places.add(snobbyShores);
-        places.add(dustyDepot);
+        int totalWeight = 0;
+        for (int i = 0 ; i < locationsFromMap.size(); i++) {
+            totalWeight += locationsFromMap.get(i).getWeight();
+        }
 
-        int result = (int) Math.round(Math.random() * places.size());
+        int index = -1;
+        int result = rand.nextInt(totalWeight);
 
-        System.out.println("Your random location is: " + places.get(result).getName());
+        for (int i = 0 ; i < locationsFromMap.size(); i++) {
+            result -= locationsFromMap.get(i).getWeight();
+
+            if (result <= 0) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("Alright boys, we're dropping at: " + locationsFromMap.get(index).getName());
     }
 }
