8bce09cbafc3f67c1abe208156d52b85c3883ea3
skwarte <skwarte@purdue.edu>
2018-10-07 22:58:42 -0500

    1 line change, fixes spinner problem

--- WHEREWELANDING/WHEREWELANDING/src/main/java/com/example/jkwak/wherewelandingfortniteedition/MainActivity.java
+++ WHEREWELANDING/WHEREWELANDING/src/main/java/com/example/jkwak/wherewelandingfortniteedition/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_welcome);
+        setContentView(R.layout.activity_main);
 
 
         spinnerArray =  new ArrayList<String>();
